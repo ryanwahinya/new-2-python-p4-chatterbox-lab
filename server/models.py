38c87eb403c28586a1a89d16ffa2e23d30b2f3f3
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-09-21 09:04:02 +0300

    fixed errors 2

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.sql import func
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,3 +13,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now(), server_default=func.now())
+
+    def __repr__(self):
+        return f"<Message {self.id} - {self.username}: {self.body}>"
